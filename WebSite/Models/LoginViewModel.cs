1cfc5305ae665e54d042f60cd47cf5d3298183d8
Zaharov Sergei <eazyfatesz@gmail.com>
2022-02-16 22:35:27 +0700

    Добавление АккаунтКонтроллера

---
+++ WebSite/Models/LoginViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace WebSite.Models
+{
+    public class LoginViewModel
+    {
+        [Required]
+        [Display(Name = "Логин")]
+        public string UserName { get; set; }
+
+        [Required]
+        [UIHint("password")]
+        [Display(Name = "Пароль")]
+        public string Password { get; set; }
+
+        [Display(Name = "Запомнить меня?")]
+        public bool RemeberMe { get; set; }
+    }
+}
